ec9c17fb9718da670a121c17bbd027de26494cb9
Mikołaj Świątek <m.swiatek@tt.com.pl>
2016-05-05 13:28:57 +0200

    Show model and cube on webpage

--- js/main.js
+++ js/main.js
@@ -1,47 +1,67 @@
-	var camera, scene, controls, renderer;
+var camera, scene, renderer, geometry, material, mesh, controls;
 
-	init();
-	animate();
+init();
+animate();
 
-	function init() {
-	    scene = new THREE.Scene();
-	    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+function init() {
 
-	    camera.position.z = 10;
+    scene = new THREE.Scene();
 
-	    var light = new THREE.DirectionalLight(0xffffff, 1);
-	    light.position.set(100, 10, 10);
-	    scene.add(light);
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 500;
+	
+	controls = new THREE.TrackballControls( camera );
+	controls.rotateSpeed = 1.0;
+	controls.zoomSpeed = 1.2;
+	controls.panSpeed = 0.8;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+	controls.keys = [ 65, 83, 68 ];
+	controls.addEventListener( 'change', render );
+	
+    scene.add(camera);
+	
+    geometry = new THREE.CubeGeometry(200, 200, 200);
+    material = new THREE.MeshNormalMaterial();
 
-	    controls = new THREE.OrbitControls(camera);
-	    controls.addEventListener('change', render);
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-	    renderer = new THREE.WebGLRenderer({alpha: true});
-	    renderer.setClearColor(0x000000, 0); // the default
-	    renderer.setSize(window.innerWidth, window.innerHeight);
-	    document.body.appendChild(renderer.domElement);
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+	
+	var loader = new THREE.JSONLoader();
+        loader.load( 'model/model.json', function ( geometry, materials ) {
+		var material = new THREE.MeshFaceMaterial(materials);
+        var human = new THREE.Mesh( geometry, materials );
+		human.position.x = 100;
+		human.position.y = 100;
+		human.position.z = 100;
+		scene.add( human );
+	}); 
 
-	    var jsonLoader = new THREE.JSONLoader();
-	    var mesh;
-	    jsonLoader.load('model/model.json',
-		function (geometry, materials) {
-		    var material = new THREE.MeshFaceMaterial(materials);
-		    mesh = new THREE.Mesh(geometry, material);
+    document.body.appendChild(renderer.domElement);
+	renderer.render(scene, camera);
+	
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.enableDamping = true;
+	controls.dampingFactor = 0.25;
+}
 
-		    scene.add(mesh);
-		}
-	    );
+function animate() {
 
-	}
+    requestAnimationFrame(animate);
+    render();
+}
 
-	function animate() {
+function render() {
 
-	    requestAnimationFrame(animate);
-	    controls.update();
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+	
+    controls.update();    
 
-	    render();
-	}
-
-	function render() {
-	    renderer.render(scene, camera);
-	}
+    renderer.render(scene, camera);
+}
