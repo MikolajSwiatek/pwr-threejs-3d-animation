f7b65caffdf9c1bb15306e6c17ee195a3065cfe9
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-05-29 20:02:14 +0200

    Refactored code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,144 @@
-var camera, scene, renderer, geometry, material, mesh, controls, trackball, statsApp, context, clock, mixer;
-var animations = [];
+var app = {
+	scene: null,
+	camera: null,
+	cube: null,
+	character: null,
+	controls: null, 
+	trackball: null, 
+	statsApp: null, 
+	context: null, 
+	clock: null, 
+	mixer: null,
+	light: null,
+	delta: null,
+	helper: new Helper(),
+	
+	init: function(fileName) {
+		this.clock = new THREE.Clock();
+		this.scene = new THREE.Scene();
+		
+		this.setCamera(50, 50, 700);
+		this.setTrackBall();
+		this.setRenderer();
+		this.setLight(new THREE.HemisphereLight(0xffffff, 0xffffff));
+		this.load(fileName);
+		this.setOrbitControls();
+		
+		document.body.appendChild(this.renderer.domElement);
+		this.renderer.render(this.scene, this.camera);
+	},
+
+	setCamera: function(x, y, z) {
+		this.camera = new THREE.PerspectiveCamera(
+			75, 
+			window.innerWidth / window.innerHeight, 
+			1, 
+			10000);
+			
+		this.camera.position.set(x, y, z);
+		this.camera.lookAt(this.scene.position);
+	},
+	
+	setTrackBall: function() {
+		this.trackball = new THREE.TrackballControls(this.camera);
+		this.trackball.rotateSpeed = 1.0;
+		this.trackball.zoomSpeed = 1.2;
+		this.trackball.panSpeed = 0.8;
+		this.trackball.noZoom = false;
+		this.trackball.noPan = false;
+		this.trackball.staticMoving = true;
+		this.trackball.dynamicDampingFactor = 0.3;
+		this.trackball.keys = [ 65, 83, 68 ];
+		this.trackball.addEventListener('change', this.render);
+	},
+	
+	setRenderer: function() {
+		this.renderer = new THREE.CanvasRenderer();
+		this.renderer.setClearColor(0xdddddd);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.shadowMapSoft = true;
+	},
+	
+	setLight: function(lightType) {
+		this.light = lightType;
+		this.scene.add(this.light);
+	},
+	
+	createCube:function(x, y, z) {
+		this.cube = new Cube()
+		this.cube.create(x, y, z, new THREE.MeshNormalMaterial());	
+	},
+	
+	load: function(fileName) {
+		var loader = new THREE.JSONLoader();
+		loader.load(this.helper.getModel(fileName), function (geometry, materials) {
+            app.character = new Character();
+			app.character.init(geometry, materials, app.helper);
+			app.character.setPosition(150, 0, 400);
+			app.saveMeshs();
+			app.setMixer();
+		});
+	},
+	
+	saveMeshs: function() {
+		if (this.character != null)
+			this.scene.add(this.character.mesh);
+			
+		if (this.cube != null)
+			this.scene.add(this.cube.mesh);
+	},
+	
+	setMixer: function() {
+		this.mixer = new THREE.AnimationMixer(this.character.mesh);
+		//for (var i=0; i<this.character.animations.length; i++) {
+		for (var i=0; i<3; i++) {
+			this.mixer.clipAction(this.character.animations[i], 0).play();
+		}
+		
+		if (this.mixer._actions[0] != undefined) {
+			this.mixer._actions[0].weight = 0;
+		}
+	},
+	
+	setOrbitControls: function() {
+		this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+		this.controls.enableDamping = true;
+		this.controls.dampingFactor = 0.25;
+	},
+	
+	animate: function() {
+		//THREE.AnimationHandler.update(clock.getDelta());
+		this.controls.update();  
+		this.delta = this.clock.getDelta();
+		this.render();
+	},
+	
+	render: function() {
+		this.renderer.render(this.scene, this.camera);
+	},
+	
+	onWindowResize: function() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}  
+};
+
+
+function Helper() {
+	this.locationFiles = {
+		model: "model/",
+		textures: "img/textures/"
+	};
+	
+	this.getModel = function(name) {
+		return this.locationFiles.model + name;
+	};
+	
+	this.getTexture = function(name) {
+		return this.locationFiles.textures + name;
+	};
+}
 
 var textures = [
     "tongue01_diffuse.png",
@@ -13,218 +152,110 @@ var textures = [
 	"tshirt02_normals.png",
 ];
 
-var LocationFiles = {
-	model: "model/",
-	textures: "img/textures/"
+function Cube() {
+	this.geometry = null,
+	this.material = null,
+	this.mesh = null,
+	
+	this.create = function(x, y, z, m) {
+		this.geometry = new THREE.CubeGeometry(x, y, z);
+		this.material = m;
+		this.mesh = new THREE.Mesh(
+			this.geometry, 
+			this.material);
+	};
+	
+	this.add = function(scene) {
+		scene.add(this.camera);
+		scene.add(this.mesh);
+	};
 };
 
-	function getModel(name) {
-		return LocationFiles.model + name;
-	}
-
-	function getTexture(name) {
-		return LocationFiles.textures + name;
-	}
+function Character() {
+	this.geometry = null;
+	this.materials = null;
+	this.animations = null;
+	this.mesh = null;
 	
-var Menu = function() {
-  //this.message = 'dat.gui';
-}
+	this.init = function(geometry, mat, helper) {
+		this.animations = geometry.animations;
+		this.geometry = geometry;
+		
+		if (mat != undefined) {
+			if (mat.length > 1) {
+				for(var i=0; i<mat.length; i++) {     
+					var texture = this.loadTexture(helper, i);       
+					mat[i] = this.getMaterial(texture);
+					mat[i].skinning = true;
+					mat[i].side = THREE.DoubleSide;
+					mat[i].shading = THREE.FlatShading;
+				}
+				
+			this.materials = mat;
+			this.mesh = new THREE.Mesh(
+				this.geometry, 
+				new THREE.MeshFaceMaterial(this.materials));
+			}
+			else {
+				mat.side = THREE.DoubleSide;
+				mat.shading = THREE.FlatShading;
+				this.materials = mat;
+				this.mesh = new THREE.SkinnedMesh(geometry,mat);
+			}
+		}
+	};
+	
+	this.getMaterial = function(material) {
+		return new THREE.MeshBasicMaterial({ map: material });
+	};
+	
+	this.loadTexture = function(helper, i) {
+		return THREE.ImageUtils.loadTexture(
+			helper.getTexture(textures[i]));
+	};
+	
+	this.setPosition = function(x, y, z) {
+		this.mesh.position.set(x, y, z);
+	};
+};
+
+function Menu() {
+	
+};
+
+var stats = {
+	statsApp: null,
+	context: null,
+	init: function() {
+		this.statsApp = new Stats();
+		statsApp.showPanel(1);
+		document.body.appendChild(statsApp.dom);
+		
+		var canvas = document.createElement('canvas');
+		canvas.width = 0;
+		canvas.height = 0;
+		document.body.appendChild(canvas);
+		
+		this.context = canvas.getContext( '2d' );
+		this.context.fillStyle = 'rgba(127,0,255,0.05)';
+	}
+};
 
 window.onload = function() {
   var text = new Menu();
   var gui = new DAT.GUI();
   //gui.add(text, 'message');
+  
+  app.createCube(100, 100, 100);
+  //app.init("model1.json");
+  app.init("trying_mirror.json");
+  app.saveMeshs();
+  animate();
 };
 
-window.addEventListener( 'resize', onWindowResize, false );  
-
-init();
-animate();
-
-function init() {
-    createClock();
-	createStats();
-    createScene();
-	createCamera();
-	createTrackball();
-	createCube();
-    //addFloor();
-	addedCubeAndCameraToScene();
-	createRenderer();
-	createLight();
-	addedHuman();
-	addedRenderer();
-	createOrbitControls();
-}
-
-    function createClock() {
-        clock = new THREE.Clock();
-    }
-
-	function createStats() {
-		var statsApp = new Stats();
-		statsApp.showPanel(1);
-		document.body.appendChild( statsApp.dom );
-		var canvas = document.createElement( 'canvas' );
-		canvas.width = 0;
-		canvas.height = 0;
-		document.body.appendChild( canvas );
-		context = canvas.getContext( '2d' );
-		context.fillStyle = 'rgba(127,0,255,0.05)';
-	}
-
-	function createScene() {
-		scene = new THREE.Scene();
-	}
-
-	function createCamera() {
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-		camera.position.set(50, 50, 700);
-		camera.lookAt(scene.position);
-	}
-
-	function createTrackball() {
-		trackball = new THREE.TrackballControls(camera);
-		trackball.rotateSpeed = 1.0;
-		trackball.zoomSpeed = 1.2;
-		trackball.panSpeed = 0.8;
-		trackball.noZoom = false;
-		trackball.noPan = false;
-		trackball.staticMoving = true;
-		trackball.dynamicDampingFactor = 0.3;
-		trackball.keys = [ 65, 83, 68 ];
-		trackball.addEventListener('change', render);
-	}
-
-	function createCube() {
-		geometry = createCubeGeometry(100, 100, 100);
-		material = new THREE.MeshNormalMaterial();
-		mesh = new THREE.Mesh(geometry, material);
-	}
-    
-        function createCubeGeometry(x, y, z) {
-            return new THREE.CubeGeometry(x, y, z);
-        }
-
-	function addedCubeAndCameraToScene() {
-		scene.add(camera);
-		scene.add(mesh);
-	}
-
-	function createRenderer() {
-		renderer = new THREE.CanvasRenderer();
-		renderer.setClearColor(0xdddddd);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.shadowMapSoft = true;
-	}
-	
-	function createLight() {
-		var hemi = new THREE.HemisphereLight(0xffffff, 0xffffff);
-		scene.add(hemi);
-	}
-    
-    function addFloor() {
-        scene.add(createFloor());
-    }
-    
-        function createFloor() {
-            var plane = new THREE.PlaneGeometry(2000, 2000, 5, 5);
-            plane.applyMatrix(new THREE.Matrix4().makeRotationX(- Math.PI/2));
-            
-            var texture = THREE.ImageUtils.loadTexture('img/desert.jpg');
-            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-            texture.repeat.set(64, 64);
-            
-            material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
-            return new THREE.Mesh(plane, material);
-        }
-
-	function addedHuman() {
-		var loader = new THREE.JSONLoader();
-		loader.load(getModel('model1.json'), function (geometry, materials) {
-            saveAnimations(geometry);
-			for(var i=0; i<materials.length; i++) {                
-                materials[i] = getMaterial(i);
-                materials[i].skinning = true;
-			}
-			createHuman(geometry, materials);
-		});
-	}
-        function saveAnimations(geometry) {
-            animations = geometry.animations;
-        }
-        
-        function getMaterial(i) {
-            return new THREE.MeshBasicMaterial({ map: loadTexture(i) });
-        }
-            
-            function loadTexture(i) {
-                return THREE.ImageUtils.loadTexture(getTexturPath(i));
-            }
-            
-                function getTexturPath(i) {
-                    return getTexture(textures[i]);
-                }
-	
-		function createHuman(geometry, materials) {
-			var human = getHumanMesh(geometry, materials);
-			scene.add(human);
-            
-            createAnimation(human, 0);
-		}
-
-			function getHumanMesh(geometry, materials) {
-				var human = new THREE.Mesh(geometry, 
-                    new THREE.MeshFaceMaterial(materials));
-				return setHumanPosition(human, -150, 0, 400);
-			}
-			
-				function setHumanPosition(human, x, y, z) {
-					human.position.set(x, y, z)
-					return human;
-				}
-         
-         function createAnimation(human, number) {
-             mixer = new THREE.AnimationMixer(human);
-					mixer.clipAction(animations[ 0 ]).play();
-         }
-
-	function addedRenderer() {
-		document.body.appendChild(renderer.domElement);
-		renderer.render(scene, camera);
-	}
-
-	function createOrbitControls() {
-		controls = new THREE.OrbitControls(camera, renderer.domElement);
-		controls.enableDamping = true;
-		controls.dampingFactor = 0.25;
-	}
+window.addEventListener( 'resize', app.onWindowResize, false );  
 
 function animate() {
-    requestAnimationFrame(animate);
-    //THREE.AnimationHandler.update( clock.getDelta() );
-    controls.update();  
-    update();
-    render();
+	requestAnimationFrame(animate);
+	app.animate();
 }
-
-	function render() {
-		//meshRotation(0.01, 0.02);
-		renderer.render(scene, camera);
-	}
-	
-		function meshRotation(x, y) {
-			mesh.rotation.x += x;
-			mesh.rotation.y += y;
-		}
-		
-	function update() {
-        var delta = clock.getDelta();
-        //THREE.AnimationHandler.update(delta);
-    }
-	
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}  
